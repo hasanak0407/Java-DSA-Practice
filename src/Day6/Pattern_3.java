050ec96007cd901838f9910fcd5ae250b4d52f6e
Hasan Khan <hakhasanahmedkhan1@gmail.com>
2025-09-11 14:06:19 +0530

    Added Day 6 Problems

---
+++ src/Day6/Pattern_3.java
@@ -0,0 +1,25 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class Pattern_3 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter n stars: ");
+        int n=sc.nextInt();
+        int sp=n-1;
+        int st=1;
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+            sp--;
+            for (int j = 1; j <= st; j++) {
+                System.out.print("*\t");
+            }
+            st++;
+            System.out.println();
+        }
+    }
+}
+
