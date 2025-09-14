2c9234eaee98fefc4cffb9b7b3f251c1cc2fc463
Hasan Khan <hakhasanahmedkhan1@gmail.com>
2025-09-14 19:33:16 +0530

    Added Day 9 Problems

--- src/Day9/Pattern_9.java
+++ src/Day9/Pattern_9.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Pattern_9 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter n: ");
+        System.out.println("Enter n:");
         int n=sc.nextInt();
 
         for(int i=1;i<=n;i++){
